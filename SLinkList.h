5bc6e3ee3dc9da044c66c979a8a4c70e70834119
nlpcvai <pubuliuyun@163.com>
2019-02-07 14:44:59 +0800

    Create SLinkList.h

---
+++ SLinkList.h
@@ -0,0 +1,17 @@
+template <class T>
+class SLinkList
+{
+private:
+    struct Tcomp
+    {
+        T data;
+        int cur;
+    };
+    Tcomp *sls;
+    int av;
+    int sl;
+    int maxlen;
+public:
+    
+
+}
