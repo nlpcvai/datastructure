d0b5c71a49d6f5799ea615ade044da2ce463bf80
nlpcvai <pubuliuyun@163.com>
2019-02-07 14:54:50 +0800

    Update SLinkList.h

--- SLinkList.h
+++ SLinkList.h
@@ -12,6 +12,17 @@ private:
     int sl;
     int maxlen;
 public:
-    
+    SLinkList(int maxsz = 100);
+    ~SLinkList();
+    void CreateList(int n);
+    void Insert(int i, T e);
+    T Delete(int i);
+    T GetElem(int i);
+    int Locate(T e);
+    int Empty();
+    int Full();
+    int Length();
+    void Clear();
+    void ListDisplay();
 
-}
+};
